af55268756a168b62bfaacf668dec2e48efb815c
mingatos <marcelo.m.toledo@gmail.com>
2013-11-04 17:59:03 -0200

    Intro view with button activated transition (modal).

---
+++ lazyDJ/lazyDJIntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  lazyDJIntroViewController.h
+//  lazyDJ
+//
+//  Created by Marcelo Toledo on 11/4/13.
+//  Copyright (c) 2013 Marcelo Mingatos de Toledo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lazyDJIntroViewController : UIViewController
+
+@end
---
+++ lazyDJ/lazyDJIntroViewController.m
@@ -0,0 +1,38 @@
+//
+//  lazyDJIntroViewController.m
+//  lazyDJ
+//
+//  Created by Marcelo Toledo on 11/4/13.
+//  Copyright (c) 2013 Marcelo Mingatos de Toledo. All rights reserved.
+//
+
+#import "lazyDJIntroViewController.h"
+
+@interface lazyDJIntroViewController ()
+
+@end
+
+@implementation lazyDJIntroViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
